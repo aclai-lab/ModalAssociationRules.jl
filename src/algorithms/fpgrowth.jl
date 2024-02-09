0382fa7a7a83d96eabee165b9499b1240f686a2b
mauro-milella <milella_mauro@live.it>
2024-02-09 18:34:34 +0100

    FPTree structure

--- src/algorithms/fpgrowth.jl
+++ src/algorithms/fpgrowth.jl
@@ -1,4 +1,39 @@
 """
+Fundamental data structure used in FP-Growth algorithm.
+Essentialy, an [`FPTree`](@ref) is a prefix tree where a root-leaf path represent an [`Itemset`](@ref).
+
+Consider the [`Itemset`](@ref)s sorted by [`gsupport`](@ref) of their items.
+An [`FPTree`](@ref) is such that the common [`Item`](@ref)s-prefix shared by different
+[`Itemset`](@ref)s is not stored multiple times.
+
+This implementation generalizes the propositional logic case scenario to modal logic;
+given two [`Itemset`](@ref)s sharing a [`Item`](@ref) prefix, they share the same path only
+if the worlds in which the they are true is the same.
+"""
+struct FPTree
+    content::Union{Nothing,Item}    # the Item contained in this node (nothing if root)
+    children::Vector{FPTree}        # children nodes
+    contributors::UInt64            # hash representing the worlds contributing to this node
+
+    function FPTree()
+        new(nothing, FPTree[], 0)
+    end
+
+    function FPTree(itemset::Itemset; isroot=false)
+        FPTree(itemset, Val(isroot))
+    end
+
+    # root constructor
+    function FPTree(itemset::Itemset, ::Val{true})
+
+    end
+
+    function FPTree(itemset::Itemset, ::Val{false})
+
+    end
+end
+
+"""
     fpgrowth(; fulldump::Bool=true, verbose::Bool=true)::Function
 
 Wrapper function for the FP-Growth algorithm over a modal dataset.
@@ -34,7 +69,7 @@ function fpgrowth(;
 
         # for each instance, sort its frequent itemsets by global support
         for i in 1:_ninstances
-            _ninstance_toitemsets_sorted[i] = sort([
+            ninstance_toitemsets_sorted[i] = sort([
                 itemset
                 for itemset in frequents
                 if getlocalmemo(miner, (:lsupport, itemset, i)) > lsupport_threshold
