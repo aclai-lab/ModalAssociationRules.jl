df6c3e3566f21db0700e868ef2cacb18a3ee290f
mauro-milella <milella_mauro@live.it>
2024-05-27 16:55:52 +0200

    minor changes

--- test/general.jl
+++ test/general.jl
@@ -213,11 +213,6 @@ _temp_arule = arules_generator(freqitems(fpgrowth_miner), fpgrowth_miner) |> fir
 lsupport(Itemset(manual_p), SoleLogics.getinstance(X2, 7); miner=fpgrowth_miner)
 lsupport(Itemset(manual_lr), SoleLogics.getinstance(X2, 7); miner=fpgrowth_miner)
 
-@test lconfidence(
-    _temp_arule, SoleLogics.getinstance(X2,7); miner=fpgrowth_miner) > 0.5
-@test gconfidence(
-    _temp_arule, dataset(fpgrowth_miner), 0.1; miner=fpgrowth_miner) > 0.5
-
 # more on Miner powerups (a.k.a, "customization system")
 @test ModalAssociationRules.initpowerups(apriori, dataset(apriori_miner)) == Powerup()
 
