69efa59ec284f5116f9c0cedb3cec28b4bd5ca43
mauro-milella <milella_mauro@live.it>
2024-11-17 17:10:54 +0100

    Minor changes

--- test/benchmark/natops-benchmark/natops-benchmark-driver.sh
+++ test/benchmark/natops-benchmark/natops-benchmark-driver.sh
@@ -2,7 +2,7 @@
 
 PROJECT_DIR="../../../"
 
-julia --project="${PROJECT_DIR}" apriori-benchmark.jl
+# julia --project="${PROJECT_DIR}" apriori-benchmark.jl
 julia --project="${PROJECT_DIR}" serial-fpgrowth-benchmark.jl
 
 for nthreads in $(seq 2 2 12); do
