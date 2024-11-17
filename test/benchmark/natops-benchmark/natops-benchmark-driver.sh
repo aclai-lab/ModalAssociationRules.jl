3bd7cb93d65920a03de19197c81682ed21460a6e
mauro-milella <milella_mauro@live.it>
2024-11-17 17:46:54 +0100

    `natops-benchmark-driver.sh` now accepts one argument

--- test/benchmark/natops-benchmark/natops-benchmark-driver.sh
+++ test/benchmark/natops-benchmark/natops-benchmark-driver.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-cd /home/mauro/.julia/dev/ModalAssociationRules/test/benchmark/natops-benchmark/
+if [ -z "$1" ]; then
+    TARGET_DIR="/home/mauro/.julia/dev/ModalAssociationRules/test/benchmark/natops-benchmark/"
+else 
+    TARGET_DIR="$1"
+fi
+
+cd "$TARGET_DIR" || { echo "Directory $TARGET_DIR not found"; exit -1; }
+
 PROJECT_DIR="../../../"
 
 # julia --project="${PROJECT_DIR}" apriori-benchmark.jl
