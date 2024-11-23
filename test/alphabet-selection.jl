dbdfcbe446a6ba7285e4e39c586e6a541627c937
mauro-milella <milella_mauro@live.it>
2024-11-23 15:26:57 +0100

    [ADD] `select_alphabet`

--- test/alphabet-selection.jl
+++ test/alphabet-selection.jl
@@ -3,6 +3,7 @@ using Distributions
 using ModalAssociationRules
 using Plots
 using Plots.Measures
+using SoleData: AbstractCondition
 using Test
 
 """
@@ -29,7 +30,7 @@ The study is returned under the form of plots. In particular:
 - `filename_metadata`=".plot": string injected before the .png extension of each plot.
 """
 function distribution_analysis(
-    X::Vector{T};
+    X::Vector{<:Vector{<:Real}};
     n_uniform_width_bins::Integer=5,
     n_quantile_bins::Integer=5,
     palette::ColorPalette=palette(:batlow10),
@@ -38,7 +39,7 @@ function distribution_analysis(
     save::Bool=true,
     savepath::String=joinpath(@__DIR__, "test", "analyses"),
     filename_metadata::String=".plt"
-) where {T<:Vector{<:Float64}}
+)
     default(palette = palette)
 
     # aggregate all the vectors in X, in one vector (this will be needed later)
@@ -61,28 +62,25 @@ function distribution_analysis(
     p2 = plot()
     μₓ, σₓ = mean(mean.(X)), mean(std.(X)) # note how this is different from mean(V), std(V)
     pdfₓ = pdf.(Normal(μₓ, σₓ), xaxis)
-    plot!(xaxis, pdfₓ, label="", title="(μ,σ) as means of (μᵢ,σᵢ)", show=false)
-
-    p3 = histogram(V, bins=30, label="", title="Histogram", alpha=0.5)
+    plot!(xaxis, pdfₓ, label="", title="N(μ,σ) as means of N(μᵢ,σᵢ)", show=false)
 
     # normal parameters of all the given values
     pdfᵥ = pdf.(Normal(μᵥ, σᵥ), xaxis)
 
     # plot equispaced discretization
     uniform_width_binedges = binedges(DiscretizeUniformWidth(n_uniform_width_bins), V)
-    p4 = plot(xaxis, pdfᵥ, label="", title="Uniform width (nbins=$(n_uniform_width_bins))")
-    vline!(p4, uniform_width_binedges, label="", color=:red, linestyle=:dash)
+    p3 = plot(xaxis, pdfᵥ, label="", title="Uniform width (nbins=$(n_uniform_width_bins))")
+    vline!(p3, uniform_width_binedges, label="", color=:red, alpha=0.75, linewidth=2)
+    histogram!(V, bins=100, label="", alpha=0.25, normalize=true)
 
     # plot quantile-based discretization
     quantile_binedges = binedges(DiscretizeQuantile(n_quantile_bins), V)
-    p5 = plot(xaxis, pdfᵥ, label="", title="Uniform area (nbins=$(n_quantile_bins))")
-    vline!(p5, quantile_binedges, label="", color=:green, linestyle=:dash)
+    p4 = plot(xaxis, pdfᵥ, label="", title="Uniform area (nbins=$(n_quantile_bins))")
+    vline!(p4, quantile_binedges, label="", color=:red, alpha=0.75, linewidth=2)
+    histogram!(V, bins=100, label="", alpha=0.25, normalize=true)
 
-    p6 = histogram(V, bins=quantile_binedges,
-        label="", title="Histogram (bins of equal area)")
-
-    layout = @layout [a b c; d e f]
-    final_plot = plot(p1, p2, p3, p4, p5, p6,
+    layout = @layout [a b; c d]
+    final_plot = plot(p1, p2, p3, p4,
         layout=layout,
         framestyle=:box,
         size=(1280, 1024),
@@ -99,11 +97,69 @@ function distribution_analysis(
     end
 end
 
+"""
+    function select_alphabet(
+        X::Vector{<:Vector{<:Real}},
+        metaconditions::Vector{<:AbstractCondition},
+        discretizers::Vector{<:DiscretizationAlgorithm}
+    )
+
+TODO
+"""
+function select_alphabet(
+    X::Vector{<:Vector{<:Real}},
+    metaconditions::Vector{<:AbstractCondition},
+    discretizers::Vector{<:DiscretizationAlgorithm}
+)
+    V = reduce(vcat, X)
+    alphabets = Dict{DiscretizationAlgorithm,Vector{<:AbstractCondition}}()
+
+    for discretizer in discretizers
+        _binedges = binedges(discretizer, V)
+
+        alphabets[discretizer] = Vector{AbstractCondition}()
+        for (condition, threshold) in Iterators.product(metaconditions, _binedges)
+            push!(
+                alphabets[discretizer],
+                ScalarCondition(condition, round(threshold, digits=2))
+            )
+        end
+    end
+
+    return alphabets
+end
+
+# driver section
 X, _ = load_NATOPS();
 
+# to generate an alphabet, we choose a variable and our metaconditions
+variable = 1
+metaconditions = [
+    ScalarMetaCondition(VariableMax(variable), <=),
+    ScalarMetaCondition(VariableMin(variable), >=)
+]
+
+# then, the number of bins and the discretization strategies
+nbins = 5
+_uniform_width_discretizer = DiscretizeUniformWidth(nbins)
+_quantile_discretizer = DiscretizeQuantile(nbins)
+discretizers = [_uniform_width_discretizer, _quantile_discretizer]
+
+# we obtain one alphabet for each strategy
+alphabets = select_alphabet(X[1:30,variable], metaconditions, discretizers)
+
+# now, we choose how to mix up all the obtained literals;
+# for example, we choose to only focus on quantile-based discretization.
+alphabet = alphabets[_quantile_discretizer]
+println("Extracted alphabet for discretizer $(_quantile_discretizer)")
+println(syntaxstring.(alphabet))
+
+# we also log a graphical report of all the binnings
 for variable in variables(X)
     distribution_analysis(
         X[1:30,variable],
+        n_uniform_width_bins=5,
+        n_quantile_bins=5,
         plot_title_variable=variable,
         plot_title_additional_info="for the first class",
         save=true,
