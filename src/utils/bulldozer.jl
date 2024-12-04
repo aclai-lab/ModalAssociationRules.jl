f8ac6654e297dd4676289e5d30bb533c03a783ad
mauro-milella <milella_mauro@live.it>
2024-12-04 18:01:15 +0100

    [ADD] minor comment

--- src/utils/bulldozer.jl
+++ src/utils/bulldozer.jl
@@ -243,6 +243,7 @@ localmemo!(
         key = LmeasMemoKey((_symbol, _armsubject, _ith_instance))
     end
 
+    # actually, lock is not necessary
     # lock(datalock(bulldozer)) do
     bulldozer.localmemo[key] = val
     # end
