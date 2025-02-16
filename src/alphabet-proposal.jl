de6dc692a9d9c89ac0f05d8bed3684c71a7e5e82
mauro-milella <milella_mauro@live.it>
2025-02-16 12:07:39 +0100

    [ADD] filters as kwargs in proposealphabet

--- src/alphabet-proposal.jl
+++ src/alphabet-proposal.jl
@@ -12,7 +12,7 @@
         nmotifs::Integer;
         r=5,
         th=0,
-        distillatemotifs=true
+        applyfilters=true
     )
 
 Propose an alphabet of propositional letters, by leveraging MatrixProfile motifs identification capabilities.
@@ -21,9 +21,13 @@ Propose an alphabet of propositional letters, by leveraging MatrixProfile motifs
 - `X::Union{Vector{Vector{<:Real}},Vector{<:Real}}}`: a representative time series, from which extract motifs; if multiple time series are provided, they are concatenated together;
 - `windowlength::Integer=10`: the length of each extracted motif;
 - `nmotifs::Integer=3`: the number of motifs to extract;
+
+# Keyword Arguments
 - `r::Integer=2`: how similar two windows must be to belong to the same motif;
 - `th::Integer=5`: how nearby in time two motifs are allowed to be;
-- `distillatemotifs::Bool`: filter out the motifs which are rarely found (less than 2 times).
+- `applyfilters::Bool=true`: decide whether to apply the filters (see the following kwargs) or not;
+- `filterbylength::Integer`: filter out the motifs which are rarely found (less than 2 times);
+- `filterbysim::Real`: TODO.
 """
 function proposealphabet(
     X::Vector{<:Vector{<:Real}},
@@ -43,13 +47,31 @@ function proposealphabet(
     nmotifs::Integer;
     r::Integer=5,
     th::Integer=0,
-    distillatemotifs::Bool=true
+    applyfilters::Bool=true,
+    kwargs...
 )
     Xmprofile = matrix_profile(X, windowlength)
     Xmotifs = motifs(Xmprofile, nmotifs; r=r, th=th)
 
-    # filter the identified motifs which are single
-    distillatemotifs && filter(motif -> length(motif.seqs) >= 2, Xmotifs)
+    applyfilters && _filteralphabet!(Xmotifs; kwargs...)
 
     return Xmotifs
 end
+
+# utility to apply a collection of filter! to an alphabet of motifs
+function _filteralphabet!(
+    Xmotifs::Vector{MatrixProfile.Motif};
+    filterbylength::Integer=2,
+    filterbysim::Real=1.0f0
+)
+    if filterbylength > 1
+        filter!(motif -> length(motif.seqs) >= filterbylength, Xmotifs)
+    end
+ 
+    if filterbysim
+        # TODO 
+    end
+
+    return Xmotifs
+end
+
