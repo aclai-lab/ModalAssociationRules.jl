107850191073963fb7c906ab9f070b5e1b8bf358
mauro-milella <milella_mauro@live.it>
2025-02-24 15:17:37 +0100

    proposealphabet docstring

--- src/alphabet-proposal.jl
+++ src/alphabet-proposal.jl
@@ -12,22 +12,24 @@
         nmotifs::Integer;
         r=5,
         th=0,
-        applyfilters=true
+        applypostprocessing=true
     )
 
-Propose an alphabet of propositional letters, by leveraging MatrixProfile motifs identification capabilities.
+Propose an alphabet of propositional letters, by leveraging `MatrixProfile` motifs identification capabilities.
 
 # Arguments
-- `X::Union{Vector{Vector{<:Real}},Vector{<:Real}}}`: a representative time series, from which extract motifs; if multiple time series are provided, they are concatenated together;
+- `X::Union{Vector{Vector{<:Real}},Vector{<:Real}}}`: a representative time series, from which extract motifs; 
+    if multiple time series are provided, they are concatenated together;
 - `windowlength::Integer=10`: the length of each extracted motif;
 - `nmotifs::Integer=3`: the number of motifs to extract;
 
 # Keyword Arguments
 - `r::Integer=2`: how similar two windows must be to belong to the same motif;
 - `th::Integer=5`: how nearby in time two motifs are allowed to be;
-- `applyfilters::Bool=true`: decide whether to apply the filters (see the following kwargs) or not;
-- `filterbylength::Integer`: filter out the motifs which are rarely found (less than 2 times);
-- `filterbysim::Real`: TODO.
+- `applypostprocessing::Bool=true`: decide whether to apply the filters (see the following kwargs) or not;
+- `filterbylength::Integer=2`: filter out the motifs which are rarely found (less than 2 times);
+- `clusterbysim::Real=1.0`: aggregate multiple motifs if they are equally informative, that is,
+    their (normalized) euclidean distance is under this threshold.
 """
 function proposealphabet(
     X::Vector{<:Vector{<:Real}},
@@ -47,13 +49,13 @@ function proposealphabet(
     nmotifs::Integer;
     r::Integer=5,
     th::Integer=0,
-    applyfilters::Bool=true,
+    applypostprocessing::Bool=true,
     kwargs...
 )
     Xmprofile = matrix_profile(X, windowlength)
     Xmotifs = motifs(Xmprofile, nmotifs; r=r, th=th)
 
-    applyfilters && _filteralphabet!(Xmotifs; kwargs...)
+    applypostprocessing && _filteralphabet!(Xmotifs; kwargs...)
 
     return Xmotifs
 end
@@ -62,13 +64,13 @@ end
 function _filteralphabet!(
     Xmotifs::Vector{MatrixProfile.Motif};
     filterbylength::Integer=2,
-    filterbysim::Real=1.0f0
+    clusterbysim::Real=1.0f0
 )
     if filterbylength > 1
         filter!(motif -> length(motif.seqs) >= filterbylength, Xmotifs)
     end
  
-    if filterbysim
+    if clusterbysim
         # TODO 
     end
 
