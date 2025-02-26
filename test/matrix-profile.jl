1537582ee40d490ea503328dfb2697d5303c7ac0
mauro-milella <milella_mauro@live.it>
2025-02-26 23:06:15 +0100

    [FIX] scalarlogiset parameterization

--- test/matrix-profile.jl
+++ test/matrix-profile.jl
@@ -11,13 +11,10 @@ using SoleData
 X, _ = load_NATOPS();
 
 # right hand y axis
-variable = 5 
+var_id = 5 
 
 # right hand in "I have command class"
-IHCC = Vector{Float32}.(X[1:30, variable]) 
-
-# the logiset we will mine
-logiset = scalarlogiset(X[1:30,:])
+IHCC = Vector{Float32}.(X[1:30, var_id]) 
 
 # parameters for matrix profile generation 
 windowlength = 20 
@@ -29,21 +26,24 @@ _motifs = motifsalphabet(IHCC, windowlength, nmotifs; r=r, th=th)
 @test length(_motifs) == 3
 # plot(_motifs)
 
-# we isolated the only variable 5 from the class "I have command",
-# thus we now have only one column/variable;
+# we isolated the only var_id 5 from the class "I have command",
+# thus we now have only one column/var_id;
 # for simplicity, let's consider also just one motif.
-motif1 = _motifs[1]
-vd1 = VariableDistance(1, motif1) # 1 because we only have 1 column/variable
+_motif = _motifs[1]
+vd1 = VariableDistance(var_id, _motif) # 1 because we only have 1 column/variable
 
-# make a proposition
-prop1 = Atom(ScalarCondition(vd1, <, 0.2))
+# make a proposition (we consider this as we entire alphabet, at the moment)
+proposition = Atom(ScalarCondition(vd1, <, 0.2))
+_items = Vector{Item}([proposition])
 
-_1_items = Vector{Item}([prop1])
+# define meaningfulness measures
+_itemsetmeasures = [(gsupport, 0.1, 0.1)]
+_rulemeasures = [(gconfidence, 0.2, 0.2)]
 
-_1_itemsetmeasures = [(gsupport, 0.1, 0.1)]
-_1_rulemeasures = [(gconfidence, 0.2, 0.2)]
+# build the logiset we will mine
+logiset = scalarlogiset(X[1:30,:], [vd1]) 
 
-fpgrowth_miner = Miner(logiset, fpgrowth, _1_items, _1_itemsetmeasures, _1_rulemeasures)
-
-
-# julia> check(prop1, SoleLogics.getinstance(logiset, 1), Interval((1.0, 2.0)) )
+# build the miner, and mine!
+fpgrowth_miner = Miner(logiset, fpgrowth, _items, _itemsetmeasures, _rulemeasures)
+mine!(fpgrowth_miner)
+@test freqitems(fpgrowth_miner) |> length == 1
