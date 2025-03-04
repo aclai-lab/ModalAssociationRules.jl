b0c54a08f600396aa04d8de2175edeb121dd18f6
mauro-milella <milella_mauro@live.it>
2025-03-04 20:51:31 +0100

    minor changes (space at end of lines)

--- test/matrix-profile.jl
+++ test/matrix-profile.jl
@@ -11,14 +11,14 @@ using SoleData
 X, _ = load_NATOPS();
 
 # right hand y axis
-var_id = 5 
+var_id = 5
 
 # right hand in "I have command class"
-IHCC = Vector{Float32}.(X[1:30, var_id]) 
+IHCC = Vector{Float32}.(X[1:30, var_id])
 
-# parameters for matrix profile generation 
-windowlength = 20 
-nmotifs = 10 
+# parameters for matrix profile generation
+windowlength = 20
+nmotifs = 10
 r = 5   # how similar two windows must be to belong to the same motif
 th = 0  # how nearby in time two motifs are allowed to be
 
@@ -31,11 +31,11 @@ _motifs = motifsalphabet(IHCC, windowlength, nmotifs; r=r, th=th)
 # for simplicity, let's consider also just one motif.
 _motif = _motifs[1]
 vd1 = VariableDistance(
-    var_id, 
-    _motif, 
-    distance=x -> 
-        size(x) == size(_motif) ? 
-        sqrt(sum([(x - _motif)^2 for (x, _motif) in zip(x,_motif)])) : 
+    var_id,
+    _motif,
+    distance=x ->
+        size(x) == size(_motif) ?
+        sqrt(sum([(x - _motif)^2 for (x, _motif) in zip(x,_motif)])) :
         maxintfloat()
 )
 
@@ -48,7 +48,7 @@ _itemsetmeasures = [(gsupport, 0.1, 0.1)]
 _rulemeasures = [(gconfidence, 0.2, 0.2)]
 
 # build the logiset we will mine
-logiset = scalarlogiset(X[1:30,:], [vd1]) 
+logiset = scalarlogiset(X[1:30,:], [vd1])
 
 # build the miner, and mine!
 fpgrowth_miner = Miner(logiset, fpgrowth, _items, _itemsetmeasures, _rulemeasures)
