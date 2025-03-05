88679c304ed89f9e7261cc2edcdcb8825ce84c27
mauro-milella <milella_mauro@live.it>
2025-03-05 11:49:27 +0100

    [ADD] test for _isdimensionally_coherent_itemset functioning

--- test/matrix-profile.jl
+++ test/matrix-profile.jl
@@ -48,17 +48,24 @@ vd2 = VariableDistance(
 )
 
 # make proposition (we consider this as we entire alphabet, at the moment)
-proposition1 = Atom(ScalarCondition(vd1, <, 2.0))
-proposition2 = Atom(ScalarCondition(vd2, <, 2.0))
+proposition1 = Atom(ScalarCondition(vd1, <, 5.0))
+proposition2 = Atom(ScalarCondition(vd2, <, 5.0))
 
-_items = Vector{Item}([proposition1, proposition2])
+# those atoms will label possibly every world; they are agnostic of their size;
+# we introduce those to test whether `isdimensionally_coherent_itemset` filter policy
+# is working later.
+vm1, vm2 = VariableMin(1), VariableMin(2)
+p = Atom(ScalarCondition(vm1, <, 999.0))
+q = Atom(ScalarCondition(vm2, <, 999.0))
+
+_items = Vector{Item}([proposition1, proposition2, p, q])
 
 # define meaningfulness measures
-_itemsetmeasures = [(gsupport, 0.1, 0.1)]
+_itemsetmeasures = [(gsupport, 0.001, 0.001)]
 _rulemeasures = [(gconfidence, 0.2, 0.2)]
 
 # build the logiset we will mine
-logiset = scalarlogiset(X[1:30,:], [vd1, vd2])
+logiset = scalarlogiset(X[1:30,:], [vd1, vd2, vm1, vm2])
 
 # build the miner, and mine!
 fpgrowth_miner = Miner(
