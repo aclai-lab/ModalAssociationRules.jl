05a67602cb17a15e89386706532e3d854b43e922
mauro-milella <milella_mauro@live.it>
2025-03-16 14:10:19 +0100

    [ADD] experiments for I Have Command (right hand)

--- test/matrix-profile.jl
+++ test/matrix-profile.jl
@@ -1,5 +1,6 @@
 using Test
 
+using DynamicAxisWarping
 using MatrixProfile
 using ModalAssociationRules
 using Plots
@@ -9,13 +10,48 @@ using Statistics
 
 using SoleData
 
+# little utility to avoid writing an experiment
+function experiment!(miner::Miner, reportname::String)
+    mine!(miner)
+
+    generaterules!(miner) |> collect
+
+    rulecollection = [
+        (
+            rule,
+            round(
+                globalmemo(miner, (:dimensional_gconfidence, rule)), digits=2
+            ),
+            round(
+                globalmemo(miner, (:dimensional_gsupport, antecedent(rule))), digits=2
+            ),
+            round(
+                globalmemo(miner, (:dimensional_gsupport, Itemset(rule))), digits=2
+            )
+        )
+        for rule in arules(miner)
+    ]
+    sort!(rulecollection, by=x->x[2], rev=true);
+
+    open(reportname, "w") do io
+        println(io, "Columns are: rule, confidence, ant support, ant+cons support")
+
+        for (rule,conf,antgsupp,consgsupp) in rulecollection
+            println(io,
+                rpad(rule, 130) * " " * rpad(string(conf), 10) * " " *
+                rpad(string(antgsupp), 10) * " " * string(consgsupp)
+            )
+        end
+    end
+end
+
 X, _ = load_NATOPS();
 
 # right hand y axis
 var_id = 5
 
 # right hand in "I have command class"
-IHCC_rhand_y_only = Vector{Float32}.(X[1:30, var_id])
+IHCC_rhand_y_only = Vector{Float64}.(X[1:30, var_id])
 
 # parameters for matrix profile generation
 windowlength = 20
@@ -31,12 +67,19 @@ mp, _raw_motifs, _motifs = motifsalphabet(
 # we isolated the only var_id 5 from the class "I have command",
 # thus we now have only one column/var_id;
 # for simplicity, let's consider also just one motif.
-normalize(x) = (x .- mean(x)) ./ std(x)
+
+# we define a distance function between two time series x, y, where |x| = |y|
 _mydistance = (x, y) -> size(x) == size(y) ?
-    # normalization on (naive, just for test)
+    # Euclidean with normalization
     # sqrt(sum([(x - y)^2 for (x, y) in zip(x |> normalize, y)])) :
-    # normalization off
-    sqrt(sum([(x - y)^2 for (x, y) in zip(x, y)])) :
+
+    # Euclidean without normalization
+    # sqrt(sum([(x - y)^2 for (x, y) in zip(x, y)])) :
+
+    # Dynamic Time Warping
+    dtw(x,y) |> first :
+
+    # distance function isz not well-defined
     maxintfloat()
 
 vd1 = VariableDistance(
@@ -57,7 +100,7 @@ vd2 = VariableDistance(
 proposition1 = Atom(ScalarCondition(vd1, <, 5.0))
 proposition2 = Atom(ScalarCondition(vd2, <, 5.0))
 
-# those atoms will label possibly every world; they are agnostic of their size;
+# those _atoms will label possibly every world (they are agnostic of their size);
 # we introduce those to test whether `isdimensionally_coherent_itemset` filter policy
 # is working later.
 vm1, vm2 = VariableMin(1), VariableMin(2)
@@ -125,7 +168,7 @@ __var__v4_l40_rhand_x_retracting_inverting_protracting = VariableDistance(4,
 
 # right hand Y variable generations
 
-_mp, _raw_motifs, _motifs_v5_l10 = motifsalphabet(IHCC[:,5], 10, 10; r=5, th=2)
+_mp, _raw_motifs, _motifs_v5_l10 = motifsalphabet(IHCC[:,5], 10, 10; r=5, th=2);
 __motif__v5_l10_rhand_y_ascending = _motifs_v5_l10[5]
 __motif__v5_l10_rhand_y_descending = _motifs_v5_l10[2]
 
@@ -141,7 +184,7 @@ __var__v5_l10_rhand_y_descending = VariableDistance(5,
 )
 
 
-_mp, _raw_motifs, _motifs_v5_l40 = motifsalphabet(IHCC[:,5], 40, 10; r=5, th=5)
+_mp, _raw_motifs, _motifs_v5_l40 = motifsalphabet(IHCC[:,5], 40, 10; r=5, th=5);
 __motif__v5_l40_rhand_y_ascdesc = _motifs_v5_l40[7]
 
 __var__v5_l40_rhand_y_ascdesc = VariableDistance(5,
@@ -196,31 +239,34 @@ variabledistances = [
     __var__v5_l10_rhand_y_descending,
     __var__v5_l40_rhand_y_ascdesc,
 
-    __var__v5_l40_rhand_z_away_hip,
-    __var__v5_l40_rhand_z_closer_hip,
+    __var__v6_l10_rhand_z_away_hip,
+    __var__v6_l10_rhand_z_closer_hip,
 ];
 
 propositional_atoms = [
     # bigger intervals' threshold can be relaxed
     Atom(ScalarCondition(__var__v4_l10_rhand_x_protracting, <, 2.0)),
     Atom(ScalarCondition(__var__v4_l10_rhand_x_retracting, <, 2.0)),
-    Atom(ScalarCondition(__var__v4_l40_rhand_x_retracting_inverting_protracting, <, 5.0)),
+    Atom(ScalarCondition(__var__v4_l40_rhand_x_retracting_inverting_protracting, <, 4.0)),
 
-    Atom(ScalarCondition(__var__v5_l10_rhand_y_ascending, <, 3.0)),
-    Atom(ScalarCondition(__var__v5_l10_rhand_y_descending, <, 3.0)),
-    Atom(ScalarCondition(__var__v5_l40_rhand_y_ascdesc, <, 5.0)),
+    Atom(ScalarCondition(__var__v5_l10_rhand_y_ascending, <, 2.0)),
+    Atom(ScalarCondition(__var__v5_l10_rhand_y_descending, <, 2.0)),
+    Atom(ScalarCondition(__var__v5_l40_rhand_y_ascdesc, <, 4.0)),
 
-    Atom(ScalarCondition(__var__v5_l40_rhand_z_away_hip, <, 3.0)),
-    Atom(ScalarCondition(__var__v5_l40_rhand_z_closer_hip, <, 3.0)),
+    Atom(ScalarCondition(__var__v6_l10_rhand_z_away_hip, <, 2.0)),
+    Atom(ScalarCondition(__var__v6_l10_rhand_z_closer_hip, <, 2.0)),
 ];
 
-atoms = reduce(vcat, [
+_atoms = reduce(vcat, [
         propositional_atoms,
-        diamond(IA_D).(propositional_atoms),
-        diamond(IA_A).(propositional_atoms)
+        diamond(IA_A).(propositional_atoms),
+        diamond(IA_L).(propositional_atoms),
+        diamond(IA_B).(propositional_atoms),
+        diamond(IA_E).(propositional_atoms),
+        diamond(IA_O).(propositional_atoms),
     ]
 )
-_items = Vector{Item}(atoms)
+_items = Vector{Item}(_atoms)
 
 _itemsetmeasures = [(dimensional_gsupport, 0.1, 0.1)]
 _rulemeasures = [(dimensional_gconfidence, 0.1, 0.1)]
@@ -244,25 +290,12 @@ apriori_miner = Miner(
     ]
 )
 
-mine!(apriori_miner)
-
-for frq in freqitems(apriori_miner)
-    println("$(frq) => gsupport $(apriori_miner.globalmemo[(:dimensional_gsupport, frq)])")
-end
-
-generaterules!(apriori_miner) |> collect
-
-rulecollection = [
-    (rule, apriori_miner.globalmemo[(:dimensional_gconfidence, rule)])
-    for rule in arules(apriori_miner)
-]
-sort!(rulecollection, by=x->x|>last, rev=true);
-
-open("results.txt", "w") do io
-    for (rule,conf) in rulecollection
-        println(io, rpad(rule, 130) * " " * string(conf))
-    end
-end
+experiment!(apriori_miner, "rhand_ihavecommand.txt")
 
 # to help debugging
 # plot([__motif__v5_l10_rhand_y_descending, IHCC[1,5][18:27] |> normalize  ])
+
+# plot frequent items in descending order by dimensiona global support
+# for frq in freqitems(miner)
+#   println("$(frq) => gsupport $(apriori_miner.globalmemo[(:dimensional_gsupport, frq)])")
+# end
