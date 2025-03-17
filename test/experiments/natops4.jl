92d8ab6e1018620ba8eacb9e12dfaab60c700286
mauro-milella <milella_mauro@live.it>
2025-03-17 21:17:03 +0100

    [ADD] elbows in experiments 4 are complete

--- test/experiments/natops4.jl
+++ test/experiments/natops4.jl
@@ -210,6 +210,24 @@ allmotifs = [
     __motif__v10_l10_relbow_right,
     __motif__v10_l30_relbow_right_reen_right,
     __motif__v10_l30_relbow_reen_right_reen,
+
+    __motif__v8_l10_lelbow_ascend,
+    __motif__v8_l10_lelbow_descending,
+    __motif__v8_l30_lelbow_ascending,
+    __motif__v8_l30_lelbow_descending,
+
+    __motif__v11_l10_relbow_descend,
+    __motif__v11_l10_relbow_ascend,
+    __motif__v11_l30_relbow_longdescend,
+    __motif__v11_l30_relbow_longascend,
+
+    __motif__v9_l10_lelbow_neutral,
+    __motif__v9_l10_lelbow_neutral,
+    __motif__v9_l30_lelbow_left_neutral,
+
+    __motif__v12_l10_relbow_right,
+    __motif__v12_l10_relbow_neutral,
+    __motif__v12_l30_relbow_right_reentering,
 ]
 
 variabledistances = [
@@ -222,6 +240,24 @@ variabledistances = [
     __var__v10_l10_relbow_left,
     __var__v10_l30_relbow_right_reen_right,
     __var__v10_l30_relbow_right_reen_right,
+
+    __var__v8_l10_lelbow_ascend,
+    __var__v8_l10_lelbow_descending,
+    __var__v8_l30_lelbow_ascending,
+    __var__v8_l30_lelbow_descending,
+
+    __var__v11_l10_relbow_descend,
+    __var__v11_l10_relbow_ascend,
+    __var__v11_l30_relbow_longdescend,
+    __var__v11_l30_relbow_longascend,
+
+    __var__v9_l10_lelbow_neutral,
+    __var__v9_l10_lelbow_neutral,
+    __var__v9_l30_lelbow_left_neutral,
+
+    __var__v12_l10_relbow_right,
+    __var__v12_l10_relbow_neutral,
+    __var__v12_l30_relbow_right_reentering,
 ];
 
 propositional_atoms = [
@@ -235,6 +271,24 @@ propositional_atoms = [
     Atom(ScalarCondition(__var__v10_l10_relbow_left, <, 4.0)),
     Atom(ScalarCondition(__var__v10_l30_relbow_right_reen_right, <, 4.0)),
     Atom(ScalarCondition(__var__v10_l30_relbow_right_reen_right, <, 4.0)),
+
+    Atom(ScalarCondition(__var__v8_l10_lelbow_ascend, <, 2.0)),
+    Atom(ScalarCondition(__var__v8_l10_lelbow_descending, <, 2.0)),
+    Atom(ScalarCondition(__var__v8_l30_lelbow_ascending, <, 4.0)),
+    Atom(ScalarCondition(__var__v8_l30_lelbow_descending, <, 4.0)),
+
+    Atom(ScalarCondition(__var__v11_l10_relbow_descend, <, 2.0)),
+    Atom(ScalarCondition(__var__v11_l10_relbow_ascend, <, 2.0)),
+    Atom(ScalarCondition(__var__v11_l30_relbow_longdescend, <, 4.0)),
+    Atom(ScalarCondition(__var__v11_l30_relbow_longascend, <, 4.0)),
+
+    Atom(ScalarCondition(__var__v9_l10_lelbow_neutral, <, 2.0)),
+    Atom(ScalarCondition(__var__v9_l10_lelbow_neutral, <, 2.0)),
+    Atom(ScalarCondition(__var__v9_l30_lelbow_left_neutral, <, 4.0)),
+
+    Atom(ScalarCondition(__var__v12_l10_relbow_right, <, 2.0)),
+    Atom(ScalarCondition(__var__v12_l10_relbow_neutral, <, 2.0)),
+    Atom(ScalarCondition(__var__v12_l30_relbow_right_reentering, <, 4.0)),
 ];
 
 _atoms = reduce(vcat, [
