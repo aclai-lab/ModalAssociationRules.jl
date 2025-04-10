2c3c5766de59a2d50d2f617c52c1f6ebfdc0f51f
mauro-milella <milella_mauro@live.it>
2025-04-10 18:45:22 +0200

    [ADD] GLMakie dependency

---
+++ test/experiments/selection-tool.jl
@@ -0,0 +1,11 @@
+# This is an experimental script to create a simple UI Tool to simplify the process
+# of manually defining an alphabet of items to start mining with.
+# This adds a heavy dependency to Makie, but will be moved to SoleXPlorer.jl one day.
+
+using ModalAssociationRules
+using GLMakie
+
+GLMakie.activate!()
+
+# TODO - read https://docs.makie.org/stable/
+# TODO - watch https://www.youtube.com/watch?v=odpoatozNz8
