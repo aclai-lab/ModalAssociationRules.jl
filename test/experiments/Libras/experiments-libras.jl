3f9427b7f77c49328a959f0c358ef0e92f6195a7
mauro-milella <milella_mauro@live.it>
2025-04-15 17:45:24 +0200

    [ADD] libras first experiment

--- test/experiments/Libras/experiments-libras.jl
+++ test/experiments/Libras/experiments-libras.jl
@@ -6,3 +6,6 @@ X, y = load_libras();
 # Experiment #1:
 ############################################################################################
 include("test/experiments/Libras/libras1.jl")
+
+println("Running experiment #1: ")
+experiment!(miner, "Libras", "v1_curved_swing.txt")
--- test/experiments/Libras/libras1.jl
+++ test/experiments/Libras/libras1.jl
@@ -1,120 +1,70 @@
 # README: see experiments-driver.jl and experiments-libras.jl
 
-# isolate "Vertical zigzag" class
-VZZ = X[1:24, :]
+# isolate "Curved swing" class
+VZZ = reduce(vcat, [X[1:12, :], X[180:192, :]])
 
 # right hand X variable generations
 
 # remember: motifsalphabet(data, windowlength, #extractions)
-_mp, _raw_motifs, _motifs_v4_l10 = motifsalphabet(
+_mp, _raw_motifs, _motifs_v1_l10 = motifsalphabet(
     VZZ[:,1], 10, 5; r=1, th=10, clipcorrection=false
 );
+__motif__v1_l10_rhand_x_right = _motifs_v1_l10[4]
+__motif__v1_l10_rhand_x_left = _motifs_v1_l10[1]
 
-__motif__v4_l10_rhand_x_right = _motifs_v4_l10[3]
-__motif__v4_l10_rhand_x_align = _motifs_v4_l10[5]
-
-__var__v4_l10_rhand_x_right = VariableDistance(4,
-    __motif__v4_l10_rhand_x_right,
-    distance=x -> expdistance(x, __motif__v4_l10_rhand_x_right),
+__var__v1_l10_rhand_x_right = VariableDistance(1,
+    __motif__v1_l10_rhand_x_right,
+    distance=x -> expdistance(x, __motif__v1_l10_rhand_x_right),
     featurename="Right"
 )
-__var__v4_l10_rhand_x_align = VariableDistance(4,
-    __motif__v4_l10_rhand_x_align,
-    distance=x -> expdistance(x, __motif__v4_l10_rhand_x_align),
-    featurename="Align"
+__var__v1_l10_rhand_x_left = VariableDistance(1,
+    __motif__v1_l10_rhand_x_left,
+    distance=x -> expdistance(x, __motif__v1_l10_rhand_x_left),
+    featurename="Left"
+)
+
+
+_mp, _raw_motifs, _motifs_v1_l40 = motifsalphabet(
+    VZZ[:,1], 40, 5; r=2, th=0, clipcorrection=false
+);
+__motif__v1_l40_rhand_x_wave = _motifs_v1_l40[4]
+
+__var__v1_l40_rhand_x_wave = VariableDistance(1,
+    __motif__v1_l40_rhand_x_wave,
+    distance=x -> expdistance(x, __motif__v1_l40_rhand_x_wave),
+    featurename="Wave"
 )
 
 # assembly the motifs here
 
 allmotifs = [
-    __motif__v4_l10_rhand_x_right,
-    __motif__v4_l10_rhand_x_align,
-    __motif__v4_l40_rhand_x_align_inverting_right,
-
-    __motif__v5_l10_rhand_y_ascending,
-    __motif__v5_l10_rhand_y_descending,
-    __motif__v5_l40_rhand_y_ascdesc,
-
-    __motif__v6_l10_rhand_z_away_front,
-    __motif__v6_l10_rhand_z_closer_front,
+    __motif__v1_l10_rhand_x_right,
+    __motif__v1_l10_rhand_x_left,
+    __motif__v1_l40_rhand_x_wave,
 ]
 
 variabledistances = [
-    __var__v4_l10_rhand_x_right,
-    __var__v4_l10_rhand_x_align,
-    __var__v4_l40_rhand_x_align_inverting_right,
-
-    __var__v5_l10_rhand_y_ascending,
-    __var__v5_l10_rhand_y_descending,
-    __var__v5_l40_rhand_y_ascdesc,
-
-    __var__v6_l10_rhand_z_away_front,
-    __var__v6_l10_rhand_z_closer_front,
+    __var__v1_l10_rhand_x_right,
+    __var__v1_l10_rhand_x_left,
+    __var__v1_l40_rhand_x_wave,
 ];
 
 # now create your alphabet
 
 propositional_atoms = [
-    Atom(ScalarCondition(
-            __var__v4_l10_rhand_x_right, <, round(suggest_threshold(
-                __motif__v4_l10_rhand_x_right, VZZ[:,4]; _percentile=5
-            ) |> first, digits=2)
-        )
-    ),
-    Atom(ScalarCondition(
-            __var__v4_l10_rhand_x_align, <, round(suggest_threshold(
-                __motif__v4_l10_rhand_x_align, VZZ[:,4]; _percentile=5
-            ) |> first, digits=2)
-        )
-    ),
-    Atom(ScalarCondition(
-            __var__v4_l40_rhand_x_align_inverting_right, <, round(suggest_threshold(
-                __motif__v4_l40_rhand_x_align_inverting_right, VZZ[:,4]; _percentile=5
-            ) |> first, digits=2)
-        )
-    ),
-
-    Atom(ScalarCondition(
-            __var__v5_l10_rhand_y_ascending, <, round(suggest_threshold(
-                __motif__v5_l10_rhand_y_ascending, VZZ[:,5]; _percentile=5
-            ) |> first, digits=2)
-        )
-    ),
-    Atom(ScalarCondition(
-            __var__v5_l10_rhand_y_descending, <, round(suggest_threshold(
-                __motif__v5_l10_rhand_y_descending, VZZ[:,5]; _percentile=5
-            ) |> first, digits=2)
-        )
-    ),
-    Atom(ScalarCondition(
-            __var__v5_l40_rhand_y_ascdesc, <, round(suggest_threshold(
-                __motif__v5_l40_rhand_y_ascdesc, VZZ[:,5]; _percentile=5
-            ) |> first, digits=2)
-        )
-    ),
-
-    Atom(ScalarCondition(
-            __var__v6_l10_rhand_z_away_front, <, round(suggest_threshold(
-                __motif__v6_l10_rhand_z_away_front, VZZ[:,6]; _percentile=5
-            ) |> first, digits=2)
-        )
-    ),
-    Atom(ScalarCondition(
-            __var__v6_l10_rhand_z_closer_front, <, round(suggest_threshold(
-                __motif__v6_l10_rhand_z_closer_front, VZZ[:,6]; _percentile=5
-            ) |> first, digits=2)
-        )
-    ),
+    Atom(ScalarCondition(__var__v1_l10_rhand_x_right, <=, 1)),
+    Atom(ScalarCondition(__var__v1_l10_rhand_x_left, <=, 1)),
+    Atom(ScalarCondition(__var__v1_l40_rhand_x_wave, <=, 2)),
 ];
 
 _atoms = reduce(vcat, [
         propositional_atoms,
-        # diamond(IA_A).(propositional_atoms),
+        diamond(IA_A).(propositional_atoms),
         # diamond(IA_L).(propositional_atoms),
-        # diamond(IA_B).(propositional_atoms),
-        # diamond(IA_E).(propositional_atoms),
-        # diamond(IA_D).(propositional_atoms),
-        # diamond(IA_O).(propositional_atoms),
+        diamond(IA_B).(propositional_atoms),
+        diamond(IA_E).(propositional_atoms),
+        diamond(IA_D).(propositional_atoms),
+        diamond(IA_O).(propositional_atoms),
     ]
 )
 _items = Vector{Item}(_atoms)
