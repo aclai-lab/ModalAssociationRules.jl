a2dd44e447c3797a899f1b9178ac391c2326aa6d
mauro-milella <milella_mauro@live.it>
2025-04-18 14:48:29 +0200

    [ADD] libras2 experiment (vertical zig-zag) setup

--- test/experiments/Libras/experiments-libras.jl
+++ test/experiments/Libras/experiments-libras.jl
@@ -9,3 +9,11 @@ include("test/experiments/Libras/libras1.jl")
 
 println("Running experiment #1: ")
 experiment!(miner, "Libras", "v1_curved_swing.txt")
+
+############################################################################################
+# Experiment #2: Vertical zig-zag
+############################################################################################
+include("test/experiments/Libras/libras2.jl")
+
+println("Running experiment #2: ")
+experiment!(miner, "Libras", "v2_vertical_zigzac.txt")
