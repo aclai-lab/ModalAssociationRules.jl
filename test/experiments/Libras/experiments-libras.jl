11ed526971b7ddc669629c725c07e133db07f578
mauro-milella <milella_mauro@live.it>
2025-04-19 13:47:40 +0200

    [ADD] making a general alphabet for libras, part 1

--- test/experiments/Libras/experiments-libras.jl
+++ test/experiments/Libras/experiments-libras.jl
@@ -14,24 +14,81 @@ CSW = reduce(vcat, [X[1:12, :], X[180:192, :]])
 S = snippets(reduce(vcat, CSW[:,1]), 5, 10; m=10)
 __motifs__v1_l10_left = [_snippet(S,1)]
 __motifs__v1_l10_right = [_snippet(S,2)]
-__motifs__v1_l10_right_to_left = [_snippet(S,3)]
+__motifs__v1_l10_right_to_left = [_snippet(S,3), _snippet(S,5)]
 __motifs__v1_l10_left_to_right = [_snippet(S,4)]
-__motifs__v1_l10_right = [_snippet(S,5)]
 
+
+__var__v1_l10_left = VariableDistance(1,
+    __motifs__v1_l10_left,
+    distance=expdistance,
+    featurename="Left"
+)
+__var__v1_l10_right = VariableDistance(1,
+    __motifs__v1_l10_right,
+    distance=expdistance,
+    featurename="Right"
+)
+__var__v1_l10_right_to_left = VariableDistance(1,
+    __motifs__v1_l10_right_to_left,
+    distance=expdistance,
+    featurename="R→L"
+)
+__var__v1_l10_left_to_right = VariableDistance(1,
+    __motifs__v1_l10_left_to_right,
+    distance=expdistance,
+    featurename="L→R"
+)
 
 # X length 20
 S = snippets(reduce(vcat, CSW[:,1]), 5, 20; m=20)
 __motifs__v1_l20_right_inv_left_inv_right = [_snippet(S,1)]
 __motifs__v1_l20_left_inv_right_inv_left = [_snippet(S,2)]
-__motifs__v1_l20_right_inv_left = [_snippet(S,3)]
-__motifs__v1_l20_left_inv_right = [_snippet(S,4), _snippet(S,5)]
+__motifs__v1_l20_left_inv_right = [_snippet(S,3)]
+__motifs__v1_l20_right_inv_left = [_snippet(S,4), _snippet(S,5)]
 
+__var__v1_l20_right_inv_left_inv_right = VariableDistance(1,
+    __motifs__v1_l20_right_inv_left_inv_right,
+    distance=expdistance,
+    featurename="R∩L∪R"
+)
+__var__v1_l20_left_inv_right_inv_left = VariableDistance(1,
+    __motifs__v1_l20_left_inv_right_inv_left,
+    distance=expdistance,
+    featurename="LUR∩R"
+)
+__var__v1_l20_left_inv_right = VariableDistance(1,
+    __motifs__v1_l20_left_inv_right,
+    distance=expdistance,
+    featurename="LUR"
+)
+__var__v1_l20_right_inv_left = VariableDistance(1,
+    __motifs__v1_l20_right_inv_left,
+    distance=expdistance,
+    featurename="R∩L"
+)
 
 # X length 40
 S = snippets(reduce(vcat, CSW[:,1]), 5, 40; m=40)
 __motifs__v1_l40_fullleft_fullright_fullleft = [_snippet(S,1)]
 __motifs__v1_l40_fullright_fullleft_fullright = [_snippet(S,2)]
 __motifs__v1_l40_right_left_right_left = [_snippet(S,3)]
+
+__var__v1_l40_fullleft_fullright_fullleft = VariableDistance(1,
+    __motifs__v1_l40_fullleft_fullright_fullleft,
+    distance=expdistance,
+    featurename="U∩"
+)
+__var__v1_l40_fullright_fullleft_fullright = VariableDistance(1,
+    __motifs__v1_l40_fullright_fullleft_fullright,
+    distance=expdistance,
+    featurename="∩U^"
+)
+__var__v1_l40_right_left_right_left = VariableDistance(1,
+    __motifs__v1_l40_right_left_right_left,
+    distance=expdistance,
+    featurename="V^V"
+)
+
 
 
 # Y length 10
@@ -54,7 +111,6 @@ S = snippets(reduce(vcat, CSW[:,2]), 5, 40; m=40)
 __motifs__v2_l40_short_movement_range = [_snippet(S,1)]
 __motifs__v2_l40_perfect_movement = [_snippet(S,2), _snippet(S,4)]
 __motifs__v2_l40_slightlyup_down_up = [_snippet(S,3), _snippet(S,5)]
-
 
 
 ############################################################################################
