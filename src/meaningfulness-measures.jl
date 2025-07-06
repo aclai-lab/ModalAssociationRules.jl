2ead7f6276f21c91c011f544cb4d047a92fd7332
mauro-milella <milella_mauro@live.it>
2025-07-06 15:32:47 +0200

    [FIX] undo local measures movement in another file

--- src/meaningfulness-measures.jl
+++ src/meaningfulness-measures.jl
@@ -300,6 +300,14 @@ _gsupport_logic = (itemset, X, threshold, miner) -> begin
 end
 
 
+_lconfidence_logic = (rule, X, ith_instance, miner) -> begin
+    _instance = getinstance(X, ith_instance)
+    num = lsupport(convert(Itemset, rule), _instance, miner)
+    den = lsupport(antecedent(rule), _instance, miner)
+
+    return Dict(:measure => num/den)
+end
+
 _gconfidence_logic = (rule, X, threshold, miner) -> begin
     _antecedent = antecedent(rule)
     _consequent = consequent(rule)
@@ -314,6 +322,13 @@ _gconfidence_logic = (rule, X, threshold, miner) -> begin
 end
 
 
+_llift_logic = (rule, X, ith_instance, miner) -> begin
+    num = lconfidence(rule, X, ith_instance, miner)
+    den = lsupport(consequent(rule), getinstance(X, ith_instance), miner)
+
+    return Dict(:measure => num/den)
+end
+
 _glift_logic = (rule, X, threshold, miner) -> begin
     num = gconfidence(rule, X, threshold, miner)
     den = gsupport(consequent(rule), X, threshold, miner)
@@ -327,6 +342,15 @@ _glift_logic = (rule, X, threshold, miner) -> begin
 end
 
 
+_lconviction_logic = (rule, X, ith_instance, miner) -> begin
+    _instance = getinstance(X, ith_instance)
+
+    num = 1 - lsupport(consequent(rule), X, _instance, miner)
+    den = 1 - lconfidence(rule, X, _instance, miner)
+
+    return Dict(:measure => num/den)
+end
+
 _gconviction_logic = (rule, X, threshold, miner) -> begin
     num = 1 - gsupport(consequent(rule), X, threshold, miner)
     den = 1 - gconfidence(rule, X, threshold, miner)
@@ -334,6 +358,16 @@ _gconviction_logic = (rule, X, threshold, miner) -> begin
     return Dict(:measure => num/den)
 end
 
+
+_lleverage_logic = (rule, X, ith_instance, miner) -> begin
+    _instance = getinstance(X, ith_instance)
+
+    _ans = lsupport(convert(Itemset, rule), X, _instance, miner) - \
+        lsupport(antecedent(rule), X, _instance, miner) * \
+        lsupport(consequent(rule), X, _instance, miner)
+
+    return Dict(:measure => _ans)
+end
 
 _gleverage_logic = (rule, X, threshold, miner) -> begin
     _ans = gsupport(convert(Itemset, rule), X, threshold, miner) -
@@ -388,6 +422,27 @@ See also [`Miner`](@ref), [`lsupport`](@ref), [`LogicalInstance`](@ref),
 
 
 """
+    function lconfidence(
+        rule::ARule,
+        ith_instance::LogicalInstance;
+        miner::Union{Nothing,AbstractMiner}=nothing
+    )::Float64
+
+Compute the local confidence for the given `rule`.
+
+Local confidence is the ratio between [`lsupport`](@ref) of an [`ARule`](@ref) on a
+[`LogicalInstance`](@ref) and the [`lsupport`](@ref) of the [`antecedent`](@ref) of the
+same rule.
+
+If a miner is provided, then its internal state is updated and used to leverage memoization.
+
+See also [`AbstractMiner`](@ref), [`antecedent`](@ref), [`ARule`](@ref),
+[`LogicalInstance`](@ref), [`lsupport`](@ref), [`Threshold`](@ref).
+"""
+@localmeasure lconfidence _lconfidence_logic
+
+
+"""
     function gconfidence(
         rule::ARule,
         X::SupportedLogiset,
@@ -411,6 +466,30 @@ See also [`antecedent`](@ref), [`ARule`](@ref), [`AbstractMiner`](@ref), [`gsupp
 
 
 """
+    function llift(
+        rule::ARule,
+        ith_instance::LogicalInstance;
+        miner::Union{Nothing,AbstractMiner}=nothing
+    )::Float64
+
+Compute the local lift for the given `rule`.
+
+Local lift measures how far from independence are `rule`'s [`antecedent`](@ref) and
+[`consequent`](@ref) on a modal logic instance.
+
+Given an [`ARule`](@ref) `X ⇒ Y`, if local lift value is around 1, then this means that
+`P(X ⋃ Y) = P(X)P(Y)`, and hence, the two [`Itemset`](@ref)s `X` and `Y` are independant.
+If value is greater than (lower than) 1, then this means that `X` and `Y` are dependant
+and positively (negatively) correlated [`Itemset`](@ref)s.
+
+If a miner is provided, then its internal state is updated and used to leverage memoization.
+
+See also [`AbstractMiner`](@ref), [`antecedent`](@ref), [`ARule`](@ref), [`glift`](@ref),
+[`LogicalInstance`](@ref), [`llift`](@ref), [`Threshold`](@ref).
+"""
+@localmeasure llift _llift_logic
+
+"""
     function glift(
         rule::ARule,
         X::SupportedLogiset,
@@ -421,6 +500,28 @@ See also [`antecedent`](@ref), [`ARule`](@ref), [`AbstractMiner`](@ref), [`gsupp
 See also [`llift`](@ref).
 """
 @globalmeasure glift _glift_logic
+
+"""
+    function lconviction(
+        rule::ARule,
+        ith_instance::LogicalInstance;
+        miner::Union{Nothing,AbstractMiner}=nothing
+    )::Float64
+
+Compute the local conviction for the given `rule`.
+
+Conviction attempts to measure the degree of implication of a rule.
+It's value ranges from 0 to +∞.
+Unlike lift, conviction is sensitive to rule direction; like lift, values far from 1
+indicate interesting rules.
+
+If a miner is provided, then its internal state is updated and used to leverage memoization.
+
+See also [`AbstractMiner`](@ref), [`antecedent`](@ref), [`ARule`](@ref),
+[`LogicalInstance`](@ref), [`llift`](@ref), [`Threshold`](@ref).
+"""
+@localmeasure lconviction _lconviction_logic
+
 
 """
     function gconviction(
@@ -435,6 +536,27 @@ See also [`lconviction`](@ref).
 @globalmeasure gconviction _gconviction_logic
 
 """
+    function lleverage(
+        rule::ARule,
+        X::SupportedLogiset,
+        threshold::Threshold;
+        miner::Union{Nothing,AbstractMiner}=nothing
+    )::Float64
+
+Compute the local leverage for the given `rule`.
+
+Measures how much more counting is obtained from the co-occurrence of the
+[`antecedent`](@ref) and [`consequent`](@ref) from the expected (from independence).
+
+This value ranges between [-0.25,0.25].
+
+See also [`AbstractMiner`](@ref), [`antecedent`](@ref), [`ARule`](@ref),
+[`consequent`](@ref), [`LogicalInstance`](@ref), [`Threshold`](@ref).
+"""
+@localmeasure lleverage _lleverage_logic
+
+
+"""
     function gleverage(
         rule::ARule,
         X::SupportedLogiset,
@@ -447,7 +569,15 @@ See also [`lleverage`](@ref).
 @globalmeasure gleverage _gleverage_logic
 
 
-# all the meaningfulness measures defined in this file are linked here,
+# All the meaningfulness measures defined in this file are linked here,
 # meaning that a global measure is associated to its corresponding local one.
 
 @linkmeas gsupport lsupport
+
+@linkmeas gconfidence lconfidence
+
+@linkmeas glift llift
+
+@linkmeas gconviction lconviction
+
+@linkmeas gleverage lleverage
