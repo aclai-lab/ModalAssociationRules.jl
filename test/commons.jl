0d1f8853c3ccf7ace4d7edc1a72a3e180965bd21
mauro-milella <milella_mauro@live.it>
2025-08-18 18:35:05 +0200

    compare(miners::Vector{AbstractMiner})

--- test/commons.jl
+++ test/commons.jl
@@ -98,7 +98,8 @@ function compare(miner1::AbstractMiner, miner2::AbstractMiner)
 end
 
 function compare(miners::Vector{AbstractMiner})
-
+    mainminer = first(miners)
+    map(targetminer -> compare(mainminer, targetminer), miners[2:end])
 end
 
 export isequal_gsupp, isequal_lsupp
