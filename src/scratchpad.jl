f929426545a4613136d0181c882649304cd0fa14
mauro-milella <milella_mauro@live.it>
2025-10-05 18:30:02 +0200

    [ADD] scratchpad code for REPL testing

---
+++ src/scratchpad.jl
@@ -0,0 +1,54 @@
+# fast copy-paste for REPL testing while developing
+
+using ModalAssociationRules
+using SoleData
+using SoleData: VariableMin, VariableMax
+using SoleData.Artifacts
+using StatsBase
+
+import ModalAssociationRules.children
+ALGORITHMS = [apriori, fpgrowth, eclat]
+
+############################################################################################
+# Toy data to parametrize experiments
+
+# load NATOPS dataset and convert it to a Logiset
+X_df, y = load(NatopsLoader());
+X_df_1_have_command = X_df[1:30, :]
+X_df_short = ((x)->x[1:4]).(X_df)
+X1 = scalarlogiset(X_df_short)
+
+X_df_1_have_command = X_df[1:30, :]
+X_1_have_command = scalarlogiset(X_df_1_have_command)
+
+# make a vector of item, that will be the initial state of the mining machine
+manual_p = Atom(ScalarCondition(VariableMin(1), >, -0.5))
+manual_q = Atom(ScalarCondition(VariableMin(2), <=, -2.2))
+manual_r = Atom(ScalarCondition(VariableMin(3), >, -3.6))
+
+manual_lp = box(IA_L)(manual_p)
+manual_lq = diamond(IA_L)(manual_q)
+manual_lr = box(IA_L)(manual_r)
+
+manual_items = Vector{Item}([
+    manual_p, manual_q, manual_r, manual_lp, manual_lq, manual_lr])
+
+manual_v2 = [
+    Atom(ScalarCondition(VariableMin(4), >=, 1))
+    Atom(ScalarCondition(VariableMin(4), >=, 1.8))
+    Atom(ScalarCondition(VariableMin(5), >=, -0.5))
+    Atom(ScalarCondition(VariableMax(6), >=, 0))
+]
+manual_v2_modal = vcat(
+    manual_v2,
+    (manual_v2)[1] |> diamond(IA_L)
+) |> Vector{Item}
+
+m = Miner(
+    X1,
+    x -> x,
+    UInt64,
+    Vector{Item}([manual_p, manual_q, manual_lp, manual_lq]),
+    [(gsupport, 0.1, 0.1)],
+    [(gconfidence, 0.0, 0.0)],
+)
