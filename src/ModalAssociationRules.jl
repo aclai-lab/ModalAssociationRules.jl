f87b801437e47469d981262aff965b1a31aa5d33
mauro-milella <milella_mauro@live.it>
2025-10-05 18:29:44 +0200

    uncommenting new part for REPL testing

--- src/ModalAssociationRules.jl
+++ src/ModalAssociationRules.jl
@@ -79,13 +79,19 @@ export generaterules, generaterules!
 
 export partial_deepcopy, miner_reduce!
 
+include("mining-policies.jl")
+
+export islimited_length_itemset, isanchored_itemset, isdimensionally_coherent_itemset
+export islimited_length_arule, isanchored_arule, isheterogeneous_arule
+export isanchored_miner
+
 include("utils/miner.jl")
 
 export Miner
 export itemtype, datatype
 export arule_analysis, all_arule_analysis
 
-include("utils/bulldozer.jl")
+include("utils/bulldozer.jl") # TODO: replace Itemset with the new type
 
 export Bulldozer
 export datalock, memolock, miningstatelock
@@ -99,54 +105,49 @@ export llift, glift
 export lconviction, gconviction
 export lleverage, gleverage
 
-include("mining-policies.jl")
-
-export islimited_length_itemset, isanchored_itemset, isdimensionally_coherent_itemset
-export islimited_length_arule, isanchored_arule, isheterogeneous_arule
-export isanchored_miner
-
-include("algorithms/apriori.jl")
-
-export combine_items, grow_prune
-export apriori
-
-include("data-structures.jl")
-
-export EnhancedItemset, count
-export ConditionalPatternBase
-
-export FPTree
-export content, parent, children, count
-export content!, parent!, children!
-export count!, addcount!
-export isroot, islist
-export itemset_from_fplist, retrieveleaf
-export grow!
-
-export HeaderTable, items
-export link, link!, follow
-export checksanity!
-
-include("algorithms/fpgrowth.jl")
-
-export patternbase, bounce!, projection
-export fpgrowth
-
-include("algorithms/eclat.jl")
-
-export eclat
-
-include("algorithms/anchored-semantics.jl")
-
-export anchored_semantics
-export anchored_grow_prune, anchored_apriori
-export anchored_fpgrowth
-
-# utilities to help an user generate an alphabet graphically;
-# see future-work folder.
-# include("alphabet-proposal.jl")
-# export motifsalphabet
-
-include("deprecate.jl")
+####
+#### include("algorithms/apriori.jl")
+####
+#### export combine_items, grow_prune
+#### export apriori
+####
+#### include("data-structures.jl")
+####
+#### export EnhancedItemset, count
+#### export ConditionalPatternBase
+####
+#### export FPTree
+#### export content, parent, children, count
+#### export content!, parent!, children!
+#### export count!, addcount!
+#### export isroot, islist
+#### export itemset_from_fplist, retrieveleaf
+#### export grow!
+####
+#### export HeaderTable, items
+#### export link, link!, follow
+#### export checksanity!
+####
+#### include("algorithms/fpgrowth.jl")
+####
+#### export patternbase, bounce!, projection
+#### export fpgrowth
+####
+#### include("algorithms/eclat.jl")
+####
+#### export eclat
+####
+#### include("algorithms/anchored-semantics.jl")
+####
+#### export anchored_semantics
+#### export anchored_grow_prune, anchored_apriori
+#### export anchored_fpgrowth
+####
+#### # utilities to help an user generate an alphabet graphically;
+#### # see future-work folder.
+#### # include("alphabet-proposal.jl")
+#### # export motifsalphabet
+####
+#### include("deprecate.jl")
 
 end
