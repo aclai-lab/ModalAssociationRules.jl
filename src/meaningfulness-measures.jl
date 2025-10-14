f44bef633807c80ef3e92cf898a0bbd71474e0c6
mauro-milella <milella_mauro@live.it>
2025-10-14 12:23:35 +0200

    [FIX] memoization power defaulted to (1<<63)-1

--- src/meaningfulness-measures.jl
+++ src/meaningfulness-measures.jl
@@ -26,8 +26,8 @@ Limit up to which memoize measures in memory.
 
 See also [`MeaningfulnessMeasure`](@ref).
 """
-LOCAL_MEMOIZATION_POWER = 1
-GLOBAL_MEMOIZATION_POWER = 1
+LOCAL_MEMOIZATION_POWER = (1<<63)-1
+GLOBAL_MEMOIZATION_POWER = (1<<63)-1
 
 """
     macro localmeasure(measname, measlogic)
