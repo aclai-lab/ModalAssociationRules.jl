c88c4839ba60d66360641dd5e96d330d313a9d6e
mauro-milella <milella_mauro@live.it>
2025-10-28 10:29:32 +0100

    [FIX] Pavia University loading as DataFrame

--- test/experiments/LandCover/experiment.jl
+++ test/experiments/LandCover/experiment.jl
@@ -1,3 +1,6 @@
+using DataFrames
+
+
 using ModalAssociationRules
 
 import SoleData: ninstances, frame
@@ -7,9 +10,16 @@ LOADER_DIRECTORY = joinpath(WORKING_DIRECTORY, "data", "land-cover.jl")
 
 include(LOADER_DIRECTORY)
 
-X_df, y = X_df, y = LandCoverDataset(
+X, y = X_df, y = LandCoverDataset(
     "Pavia University";
     window_size          = 3,
     ninstances_per_class = 40,
     pad_window_size      = 5,
 );
+X_perm = permutedims(X, (1,2,4,3))
+
+# the size of X_df is (103, 360)
+X_df = [DataFrame(X[:, :, i, j], :auto) for i in axes(X,3), j in axes(X,4)]
+
+# the size of X_df_perm is (360, 103), which is in line with y column vector
+X_df_perm = [DataFrame(X_perm[:, :, i, j], :auto) for i in axes(X,4), j in axes(X,3)]
